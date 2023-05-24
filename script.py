e74c035157db72a909999e8ea3191baf9e36cd94
theubie <randell@nvcreativetechnology.com>
2023-05-24 14:55:54 -0500

    Fixed to work with current commit of Oooba.

--- script.py
+++ script.py
@@ -3,6 +3,7 @@ import pickle
 import modules.shared as shared
 from modules.extensions import apply_extensions
 from modules.text_generation import encode, get_max_prompt_length
+from modules.chat import generate_chat_prompt
 
 try:
     with open('saved_data.pkl', 'rb') as f:
@@ -15,46 +16,14 @@ except FileNotFoundError:
     }
 
 
-def custom_generate_chat_prompt(user_input, max_new_tokens, name1, name2, context, chat_prompt_size, impersonate=False, also_return_rows=False):
+def custom_generate_chat_prompt(user_input, state, **kwargs):
 
+    print(f"user_input: {user_input}\nstate: {state}\nparams: {params}")
     if params['activate']:
-        rows = [f"{params['simple memory'].strip()}\n{context.strip()}\n"]
-    else:
-        rows = [f"{context.strip()}\n"]
+        state["context"] = f"{params['simple memory'].strip()}\n {state['context'].strip()} \n"
 
-    if shared.soft_prompt:
-        chat_prompt_size -= shared.soft_prompt_tensor.shape[1]
-    max_length = min(get_max_prompt_length(max_new_tokens), chat_prompt_size)
+    return generate_chat_prompt(user_input, state, **kwargs)
 
-    i = len(shared.history['internal']) - 1
-    while i >= 0 and len(encode(''.join(rows), max_new_tokens)[0]) < max_length:
-        rows.insert(1, f"{name2}: {shared.history['internal'][i][1].strip()}\n")
-        if not (shared.history['internal'][i][0] == '<|BEGIN-VISIBLE-CHAT|>'):
-            rows.insert(1, f"{name1}: {shared.history['internal'][i][0].strip()}\n")
-        i -= 1
-
-    if not impersonate:
-        rows.append(f"{name1}: {user_input}\n")
-        rows.append(apply_extensions(f"{name2}:", "bot_prefix"))
-        limit = 3
-    else:
-        rows.append(f"{name1}:")
-        limit = 2
-
-    while len(rows) > limit and len(encode(''.join(rows), max_new_tokens)[0]) >= max_length:
-        rows.pop(1)
-
-    prompt = ''.join(rows)
-    if also_return_rows:
-        return prompt, rows
-    else:
-        return prompt
-
-
-def input_modifier(string):
-    if not any((shared.args.chat, shared.args.cai_chat)):
-        string = f"{params['simple memory'].strip()}\n{string}"
-    return string
 
 
 def ui():
